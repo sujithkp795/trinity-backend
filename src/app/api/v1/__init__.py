11984c8ceae542fd8f49291c879f7d0ee22400c9
sujithkp795 <pendem.s@geekyants.com>
2024-12-01 03:58:16 +0530

    Cleanup code

--- src/app/api/v1/__init__.py
+++ src/app/api/v1/__init__.py
@@ -2,20 +2,13 @@ from fastapi import APIRouter
 
 from .login import router as login_router
 from .logout import router as logout_router
-from .posts import router as posts_router
-from .rate_limits import router as rate_limits_router
-from .tasks import router as tasks_router
-from .tiers import router as tiers_router
 from .users import router as users_router
 from .chat import router as chat_router
 from .conversations import router as conversations_router
+
 router = APIRouter(prefix="/v1")
 router.include_router(login_router)
 router.include_router(logout_router)
 router.include_router(users_router)
-router.include_router(posts_router)
-router.include_router(tasks_router)
-router.include_router(tiers_router)
-router.include_router(rate_limits_router)
 router.include_router(chat_router)
 router.include_router(conversations_router)
